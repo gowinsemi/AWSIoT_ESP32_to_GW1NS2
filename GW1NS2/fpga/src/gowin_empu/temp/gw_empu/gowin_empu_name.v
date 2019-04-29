454c361c849535bffca86e868cdc5d91034e57c9
gowinsemi <47979992+gowinsemi@users.noreply.github.com>
2019-04-29 15:35:17 -0500

    replacing with working fpga design

--- GW1NS2/fpga/src/gowin_empu/temp/gw_empu/gowin_empu_name.v
+++
@@ -1,2 +0,0 @@
-`define module_name_empu Gowin_EMPU
-`define getname(oriName,tmodule_name) \~oriName.tmodule_name
--- GW1NS2/fpga/src/gowin_empu/temp/gw_empu/synlog.tcl
+++
@@ -1,2 +0,0 @@
-project -load C:/Users/Grant/Documents/Gowin/DK-START-GW1NS2-LED-test-demo/fpga/src/gowin_empu/temp/gw_empu/gw_empu.prj
-project -run synthesis 
